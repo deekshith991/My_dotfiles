2f788d026148acad7fe87786913df702299c2103
vaggu deekshith <vaggudeekshith@outlook.com>
2024-12-22 19:01:10 +0530

    [+] init lua

---
+++ config/.config/nvim/init.lua
@@ -0,0 +1,10 @@
+
+-- entry point for lazy vim
+-- require("config.lazy")
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader= " "
+
