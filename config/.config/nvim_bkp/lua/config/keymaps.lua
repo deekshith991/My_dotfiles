6ae2fefbc33b14d4e865ad6b9ab5af1cdd7a9823
vaggu deekshith <vaggudeekshith@outlook.com>
2024-12-22 18:14:26 +0530

    [+] creating bkp

---
+++ config/.config/nvim_bkp/lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("n", "qq", ":q<CR>")
+
+vim.keymap.set("n", "QQ", ":q!<CR>")
+
+vim.keymap.set("n", "WQ", ":wq<CR>")
